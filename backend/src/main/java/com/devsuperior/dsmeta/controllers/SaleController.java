b3a00c5c7f25858932b97dff6231bcca486060ba
Felipe Sousa <felipe.fps09@hotmail.com>
2022-07-13 20:58:10 -0300

    Twilio sms

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -5,12 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,13 +20,20 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findPagination(
 			@RequestParam(value = "minDate", defaultValue = "") String minDate,
-			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
-			Pageable pageable) {
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
 		Page<SaleDTO> page = service.findAllPaged(minDate, maxDate, pageable);
 		return ResponseEntity.ok().body(page);
+	}
+
+	@GetMapping("/{id}/notifications")
+	public void notifyPerSms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
