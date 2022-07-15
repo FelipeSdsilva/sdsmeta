092760fa77e9ee6ef38c98a470c89622df38aa54
Felipe Sousa <felipe.fps09@hotmail.com>
2022-07-15 19:22:50 -0300

    Lista de vendas

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,6 +4,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import './styles.css';
 import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/sales';
 
 export const SalesCard = () => {
 
@@ -12,12 +14,14 @@ export const SalesCard = () => {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
-    useEffect(()=>{
-        axios.get("http://localhost:8080/sales")
-        .then(response =>{
-            console.log(response.data);
-        })
-    },[]);
+    const [sales, setSales] = useState<Sale[]>([]);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => {
+                setSales(response.data.content);
+            })
+    }, []);
 
     return (
         <>
@@ -55,83 +59,21 @@ export const SalesCard = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">28/06/2022</td>
-                                <td>Felipe Sousa</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
+                            {sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <NotificationButton />
+                                        </td>
+                                    </tr>);
+                            })}
+
                         </tbody>
                     </table>
                 </div>
---
+++ frontend/src/models/sales.ts
@@ -0,0 +1,8 @@
+export type Sale = {
+    amount: number;
+    date: string;
+    deals: number;
+    id: number;
+    sellerName: string;
+    visited: number;
+}
